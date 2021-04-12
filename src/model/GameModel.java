8d7369b31abdd261da83fef403223017cd504945
Vasilis <vasiliskoutsobasilis@gmail.com>
2021-04-12 11:58:52 +0300

    Testing for pull

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -2,57 +2,6 @@ package model;
 
 
 public class GameModel {
-	private PLayerCatalogue playerCatalogue;
-	private String[] gamePlayers;
-	private String[][] gameBoard;
-	
-	public GameModel() {
-		this.playerCatalogue = new PLayerCatalogue();
-		this.gamePlayers = new String[2];
-		this.gameBoard = null;
-	}
-	
-	public void selectPlayer(String player, int pos) {
-		if(pos < 0 && pos > 4)
-			return;
-		else
-			this.gamePlayers[pos] = player;
-	}
-	
-	public boolean ready() {
-		return (this.gamePlayers[0] != null && this.gamePlayers[1] != null);
-	}
-	
-	public void startGame() {
-		this.gameBoard = new String[3][3];
-	}
-	
-	public boolean inPlay() {
-		return (gameBoard != null);
-	}
-	
-	public boolean noPlay() {
-		return !inPlay();
-	}
-	
-	public String[] getPlayers() {
-		return this.gamePlayers;
-	}
-	
-	public String[][] getGameBoard(){
-		return this.gameBoard;
-	}
-	
-	public void setGameBoard(String[][] gameBoard) {
-		this.gameBoard = gameBoard;
-	}
-	
-	public PLayerCatalogue getPlayerCatalogue() {
-		return playerCatalogue;
-	}
 
-	public void setPlayerCatalogue(PLayerCatalogue playerCatalogue) {
-		this.playerCatalogue = playerCatalogue;
-	}
 
 }
